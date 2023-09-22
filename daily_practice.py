a55f9d30633a82e357c09361e651d4fff8f4a264
LKP <clapton79@gmail.com>
2023-09-22 08:58:52 +0200

    configured daily practice

--- daily_practice.py
+++ daily_practice.py
@@ -8,8 +8,15 @@ def daily_routine():
         v.output_decorator("FILE: {0}".format(file), 2)
         v.load_file(f'vocabularies/{file}')
         v.word_memorizer(20)
-        v.test_2(15)
-        v.test_1(10)
+        v.test_2('n', 15)
+        v.test_2('a', 15)
+        v.test_2('v', 15)
+        v.test_2('s', 15)
+
+        v.test_1('n', 15)
+        v.test_1('a', 15)
+        v.test_1('v', 15)
+        v.test_1('s', 15)
         v.unload_vocabulary()
         if file != files[-1]:
                 print("5 seconds till the next round.")
