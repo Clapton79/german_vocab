2ea480aa2cd463f1f0c082149a69352ace199c11
LKP <clapton79@gmail.com>
2023-10-04 17:26:03 +0200

    added defs

--- quality.py
+++ quality.py
@@ -4,26 +4,42 @@ import pandas as pd
 
 files = os.listdir('vocabularies')
 
-for file in files:
-    v.load_file(f'vocabularies/{file}')
-    v.output_decorator("FILE: {0}".format(file), 2)
-   
-    v.output_decorator("Words without mode info",6)
-    a=v.df[pd.isnull(v.df['mode'])]
-    print(len(a))
-    if len(a)>0:
-        print(a)
+def file_dq():
+    for file in files:
+        v.load_file(f'vocabularies/{file}')
+        v.output_decorator("FILE: {0}".format(file), 2)
+    
+        v.output_decorator("Words without mode info",6)
+        a=v.df[pd.isnull(v.df['mode'])]
+        print(len(a))
+        if len(a)>0:
+            print(a)
 
-    v.output_decorator("Words with punctuation but not of the sentence type",6)
-    a=v.df[v.df['translation'].str.contains('[\.\!\?]',regex=True, na=False)][v.df['mode']!='s']
-    print(len(a))
-    if len(a)>0:
-        print(a)
+        v.output_decorator("Words with punctuation but not of the sentence type",6)
+        a=v.df[v.df['translation'].str.contains('[\.\!\?]',regex=True, na=False)][v.df['mode']!='s']
+        print(len(a))
+        if len(a)>0:
+            print(a)
 
-    v.output_decorator("Sentences with no punctuation",6)
-    a = v.df[~v.df['translation'].str.contains('[\.\!\?]',regex=True, na=False)][v.df['mode']=='s']
-    print(len(a))
-    if len(a)>0:
-        print(a)
+        v.output_decorator("Sentences with no punctuation",6)
+        a = v.df[~v.df['translation'].str.contains('[\.\!\?]',regex=True, na=False)][v.df['mode']=='s']
+        print(len(a))
+        if len(a)>0:
+            print(a)
 
-    v.unload_vocabulary()
+        v.unload_vocabulary()
+
+
+def mesh_all() -> pd.DataFrame:
+    d = pd.DataFrame()
+    for file in files:
+        dv = pd.read_csv(f'vocabularies/{file}')
+        dv['file']=file
+        d = d._append(dv)
+
+    return d
+    
+
+
+
+
