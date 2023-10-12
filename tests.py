458125637b65b3f228328b9d7ec620ed330a5c05
LKP <clapton79@gmail.com>
2023-10-12 22:30:37 +0200

    updated to use current file

--- tests.py
+++ tests.py
@@ -1,8 +1,5 @@
 import vocabularizer as v
 
 v.get_version()
-v.load_file(f'vocabularies/German_English_A11Chapter1.csv')
-v.test_1('a',5)
-
-
-v.unload_vocabulary()
+v.load_file(f'vocabularies/German_English_202310.csv')
+v.inspect_vocabulary()
