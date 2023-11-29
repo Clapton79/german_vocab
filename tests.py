b0f700faa2566ec06442f538fa577b47fcd7ef3d
LKP <clapton79@gmail.com>
2023-11-29 16:49:19 +0100

    updated test file

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 import vocabularizer as v
 
 v.get_version()
+v.load_file(f'vocabularies/German_English_202311.csv')
+v.load_file(f'vocabularies/German_English_202309.csv')
 v.load_file(f'vocabularies/German_English_202310.csv')
-v.inspect_vocabulary()
+#v.inspect_vocabulary()
+v.test_3(25)
