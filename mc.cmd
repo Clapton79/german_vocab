119a81de2ab216db89034d59f7197de15bf16e5e
lkp <clapton79@gmail.com>
2025-07-11 20:17:59 +0200

    checkpoint

--- mc.cmd
+++ mc.cmd
@@ -3,7 +3,7 @@ alias gva="python3 /Users/lpal/Documents/Private/German/german_vocab/daily_routi
 alias gvd="python3 /Users/lpal/Documents/Private/German/german_vocab/daily_routine_tests.py"
 alias gvq="python3 /Users/lpal/Documents/Private/German/german_vocab/daily_routine_query_dict.py"
 # Activate German vocab virtual environment for python
-alias venv="source /Users/lpal/Documents/Private/German/german_vocab/vocabenv/bin/activate"
+alias venv="source /Users/lpal/Documents/Private/German/german_vocab/venv/bin/activate"
 
 # use python instead of python3
 alias python="python3"
