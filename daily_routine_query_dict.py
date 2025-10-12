7f40374f06c2f37cbe387b29401abd7781a58e7a
lkp <clapton79@gmail.com>
2025-10-12 17:53:39 +0200

    cleanup of unused code

--- daily_routine_query_dict.py
+++
@@ -1,9 +0,0 @@
-from vocab import *
-
-base_v = Vocabulary('dict.yaml')
-
-print(f"All tags: {base_v.tags()}")
-tag_filter = input('Tag: ')
-
-print([word for word, detail in base_v.vocab.items()
-      if tag_filter in detail.get('tags', [])])
